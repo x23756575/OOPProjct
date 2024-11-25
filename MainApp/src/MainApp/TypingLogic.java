4b0c1626df05e2a9b7c306fc097221fc060029a7
23756575 <samoreillyashbourne@icloud.com>
2024-11-25 15:48:49 +0000

    I added the color change code to my files e.g when user enters text. If a char is correct it will display green and if its wrong, red. solved alot of issues with bugs where text and timer were not changing when game was reset

--- MainApp/src/MainApp/TypingLogic.java
+++ MainApp/src/MainApp/TypingLogic.java
@@ -1,5 +1,9 @@
 package MainApp;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
@@ -14,6 +18,11 @@ public class TypingLogic implements KeyListener{
     boolean pressed;
     private int count = 10; 
     private Timer timer;
+    private String currentText; 
+    private JLabel label;
+
+    StringBuilder userInput = new StringBuilder();
+    StringBuilder newText = new StringBuilder("<html>");
     
     public TypingLogic(MainGUI mp) {
         this.mp = mp;
@@ -24,20 +33,28 @@ public class TypingLogic implements KeyListener{
         mp.requestFocusInWindow(); //I added this as a test because keylistener was having problems.
         //I was able to figure setFocusable(true) was necessary as the gameJPanel needed focus form keybaord inputs
         mp.addKeyListener(this);
+        mp.setFocusTraversalKeysEnabled(false);
+
         
         JLabel time = new JLabel(String.valueOf(count));
         time.setBounds(50, 50, 100, 30);
         gameJPanel.add(time);
         System.out.println("Panel is focusable: " + gameJPanel.isFocusable());
+        JLabel typingLabel = typingText(); // Get the JLabel with random text
+        currentText = typingLabel.getText();
 
-        timer = new Timer(1000,e -> {
-          count--;
-          
-          time.setText(String.valueOf(count));//this will update the label every 1000 ms
-           if(count <= 0){//ends loop if less than or equal to 0
-           ((Timer) e.getSource()).stop(); // Stop the timer when it reaches 0
-           }
+        timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                count--;
+                time.setText(String.valueOf(count));
+                if (count <= 0) {
+            ((Timer) e.getSource()).stop();
+            loopTest();
+                }
+            }
         });
+
 
         gameJPanel.revalidate();
         gameJPanel.repaint();
@@ -72,31 +89,106 @@ public class TypingLogic implements KeyListener{
     "the purpose of life is not to be happy"
 };
         Random random = new Random();
-        int index = random.nextInt(sentences.length); // Picks a random sentence from the array
-        JLabel label = new JLabel(sentences[index]);  // Create JLabel with the random sentence
+        int index = random.nextInt(sentences.length); // this picks a random sentence from the array
+        label = new JLabel(sentences[index]); 
+        label.setForeground(Color.black);// this is the default color of the text that i will display from the above array
 
-        return label; // Returns the label
-        
-
+        return label;
 
     }
-            @Override
-        public void keyTyped(KeyEvent e) {
-          
-        }
+public void loopTest() {
+    timer.stop(); // Stop the timer
+    count = 10;   // Reset the countdown
+    
+    currentText = typingText().getText(); // Get a new random sentence
+    userInput.setLength(0); // Clear the user input
 
-        @Override
-        public void keyPressed(KeyEvent e) {
-            System.out.println("Key pressed: " + e.getKeyChar());
+    // Build the new HTML for the fresh sentence
+    newText.setLength(0);
+    newText.append("<html>");
+    for (int i = 0; i < currentText.length(); i++) {
+        newText.append("<span style='color:black;'>")
+               .append(currentText.charAt(i))
+               .append("</span>");
+    }
+    newText.append("</html>");
 
-           pressed = true;
-            timer.start();
-            System.out.println("Timer started!");
-         
+    label.setText(newText.toString()); // Update the label text
+    mp.getGameJPanel().add(label);
+    pressed = false; // Reset the pressed flag
+label.setFont(new Font("arial", Font.BOLD, 20));
+label.setBounds(270, 100, 900, 50); // Adjust as necessary
+mp.getGameJPanel().add(label);
+
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+@Override
+
+public void keyTyped(KeyEvent e) {
+    char key = e.getKeyChar();
+
+    // Append the typed character to userInput
+    if (userInput.length() < currentText.length()) {
+        userInput.append(key);
+    }
+
+    // Clear the newText builder for fresh HTML construction
+    newText.setLength(0);
+    newText.append("<html>");
+
+    for (int i = 0; i < currentText.length(); i++) {
+        if (i < userInput.length()) {
+            // Correctly typed character
+            if (currentText.charAt(i) == userInput.charAt(i)) {
+                newText.append("<span style='color:green;'>")
+                       .append(currentText.charAt(i))
+                       .append("</span>");
+            } else {
+                // Incorrectly typed character
+                newText.append("<span style='color:red;'>")
+                       .append(currentText.charAt(i))
+                       .append("</span>");
+            }
+        } else {
+            // Untyped characters in black
+            newText.append("<span style='color:black;'>")
+                   .append(currentText.charAt(i))
+                   .append("</span>");
         }
+    }
+
+
+    newText.append("</html>");
+
+    // Update the label with the new colored HTML text
+    label.setText(newText.toString());
+    timer.start();
+
+}
+
         @Override
         public void keyReleased(KeyEvent e) {
         }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+          if(userInput.length() == currentText.length()){
+        if(e.getKeyCode() == KeyEvent.VK_TAB){
+            System.out.println("tab");
+            label.setText("");
+       loopTest();
+        }
+    }
+    }
 }
 
 
