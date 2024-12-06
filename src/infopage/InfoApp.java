3f7d1dbc45b69b85f744deadb933ad8f5305f5dd
Saboteur <79015406+Saboteur0201@users.noreply.github.com>
2024-12-06 15:38:01 +0000

    Update InfoApp.java

--- src/infopage/InfoApp.java
+++ src/infopage/InfoApp.java
@@ -1,5 +1,7 @@
 package infopage;
 
+import javax.swing.JPanel;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -15,5 +17,23 @@ public class InfoApp{
        
         InfoForm infoForm = new InfoForm();
         infoForm.setVisible(true); 
+        InfoFrame infoframe = new InfoFrame();
+        infoframe.setVisible(true);
+        
+    }
+    
+    private MainGUI mp;
+    
+    public InfoApp(MainGUI mp){
+        this.mp = mp;
+    
+        JPanel infoJPanel = mp.getInfoJPanel();
+
+        infoJPanel.revalidate();
+        infoJPanel.repaint();
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
